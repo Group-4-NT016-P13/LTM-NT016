afdf8dbfa08975f73fb14d42749c93c72709ac63
khongbietcode <23521383@gm.edu.uit.vn>
2024-10-29 14:08:42 +0700

    menu cho game

---
+++ DoAn2/Chess/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    public partial class Menu : Form
+    {
+        private string UserNickname;
+        private string UserEmail;
+        public Menu()
+        {
+            InitializeComponent();
+        }
+        public Menu(string Nickname, string Email)
+        {
+            InitializeComponent();
+            ToolTip toolTip = new ToolTip();
+            string toolTipText = $"Tên người dùng: {Nickname}\nEmail: {Email}";
+            toolTip.SetToolTip(UserInfo, toolTipText);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserInfo_Click(object sender, EventArgs e)
+        {
+            Infor log = new Infor(UserNickname,UserEmail);
+            log.Show();
+            this.Hide();
+        }
+    }
+}
