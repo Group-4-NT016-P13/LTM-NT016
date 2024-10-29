a2d4547f667c1a9ed517fee0383c3e0912eabdf4
khongbietcode <23521383@gm.edu.uit.vn>
2024-10-29 14:00:16 +0700

    updateMerge branch 'main' of https://github.com/Group-4-NT016-P13/LTM-NT016

---
+++ DoAn2/Chess/Signup.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    public partial class Signup : Form
+    {
+        private bool isPasswordVisible = false;
+        public static string Passdecode(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+        public static bool checkmail(string email)
+        {
+            string Pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, Pattern);
+        }
+
+        public Signup()
+        {
+            InitializeComponent();
+        }
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                SelectNextControl((Control)sender, true, true, true, true);
+            }
+        }
+
+        private void Username_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Username_txt.Text))
+            {
+
+                Username_lb.Visible = true;
+            }
+            else
+            {
+
+                Username_lb.Visible = false;
+            }
+        }
+        private void Nickname_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Nickname_txt.Text))
+            {
+
+                Nickname_lb.Visible = true;
+            }
+            else
+            {
+
+                Nickname_lb.Visible = false;
+            }
+        }
+        private void Email_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Email_txt.Text))
+            {
+
+                Email_lb.Visible = true;
+            }
+            else
+            {
+
+                Email_lb.Visible = false;
+            }
+        }
+        private void Comfirm_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Comfirm_txt.Text))
+            {
+
+               Comfirm_lb.Visible = true;
+            }
+            else
+            {
+
+                Comfirm_lb.Visible = false;
+            }
+        }
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Password_txt.Text))
+            {
+
+                Password_lb.Visible = true;
+            }
+            else
+            {
+
+                Password_lb.Visible = false;
+            }
+        }
+
+        private void ShowPassword_btn_Click_1(object sender, EventArgs e)
+        {
+            isPasswordVisible = !isPasswordVisible;
+
+            if (isPasswordVisible)
+            {
+                Password_txt.PasswordChar = '\0';
+            }
+            else
+            {
+                Password_txt.PasswordChar = '*';
+            }
+        }
+
+        private void ShowComfirm_btn_Click(object sender, EventArgs e)
+        {
+            isPasswordVisible = !isPasswordVisible;
+
+            if (isPasswordVisible)
+            {
+                Comfirm_txt.PasswordChar = '\0';
+            }
+            else
+            {
+                Comfirm_txt.PasswordChar = '*';
+            }
+        }
+
+        private void Return_btn_Click(object sender, EventArgs e)
+        {
+            Login log = new Login();
+            log.Show();
+            this.Hide();
+        }
+
+        private void Signup_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
